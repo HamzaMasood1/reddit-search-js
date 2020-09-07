8c88008f1b7517694446c546485bff3d4ed15c55
HamzaMasood1 <hamzamasood183@gmail.com>
2020-09-07 21:47:28 +0100

    added error handling

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const searchForm = document.getElementById('search-form');
 const searchInput = document.getElementById('search-input');
 
+
+//Form event listener
 searchForm.addEventListener('submit', (e) => {
     //get search term
     const searchTerm = searchInput.value;
@@ -13,6 +15,29 @@ searchForm.addEventListener('submit', (e) => {
     const searchLimit = document.getElementById('limit').value;
     console.log(searchLimit);
 
+    //check input
+    if (searchTerm == '') {
+        showMessage('Please add a search term', 'alert-danger');
+    }
     e.preventDefault();
 
-})
+});
+
+//show message
+function showMessage(message, className) {
+    //create div
+    const div = document.createElement('div');
+    //add classes
+    div.className = `alert ${className}`;
+    //Add text
+    div.appendChild(document.createTextNode(message));
+    //get parent container
+    const searchContainer = document.getElementById('search-container');
+    //get search
+    const search = document.getElementById('search');
+    //insert message
+    searchContainer.insertBefore(div, search);
+    //Timeout alert
+    setTimeout(() => document.querySelector('.alert').remove(), 3000);
+
+}
